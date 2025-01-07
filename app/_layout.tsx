ee1c727f53979cfbcd6c20bb483638966591a341
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2025-01-07 18:44:19 +0600

    fonts work in web but not in expo app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,12 @@ import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    Rubik_Regular: require("../assets/fonts/Rubik-Regular.ttf"),
-    Rubik_Medium: require("../assets/fonts/Rubik-Medium.ttf"),
-    Rubik_SemiBold: require("../assets/fonts/Rubik-SemiBold.ttf"),
-    Rubik_Bold: require("../assets/fonts/Rubik-Bold.ttf"),
-    Rubik_ExtraBold: require("../assets/fonts/Rubik-ExtraBold.ttf"),
-    Rubik_Light: require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
   });
 
   useEffect(() => {
